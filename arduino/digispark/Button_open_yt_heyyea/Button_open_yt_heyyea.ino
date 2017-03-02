6eec98982b62e31d54303a7a94be0323ba57a96b
kaliumxyz <martijn@becker.wtf>
2017-03-02 20:46:29 +0100

    Add files via upload
    Standard hello_world; same with a button; and a fun script;

---
+++ arduino/digispark/Button_open_yt_heyyea/Button_open_yt_heyyea.ino
@@ -0,0 +1,25 @@
+// the setup routine runs once when you press reset:
+// Dit gebruikt de hello button layout.
+#include "DigiKeyboard.h"
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(0, OUTPUT); //LED on Model B
+  pinMode(1, OUTPUT); //LED op dit model!   
+  pinMode(2, INPUT);  // Input for button
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(0, HIGH); // powah to the button
+  if (digitalRead(2)){
+    DigiKeyboard.update();
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+    delay(100);
+    DigiKeyboard.print("https://www.youtube.com/watch?v=ZZ5LpwO-An4");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  }
+  
+  Serial.println(digitalRead(2));
+}
---
+++ arduino/digispark/Hello_button/Hello_button.ino
@@ -0,0 +1,18 @@
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(0, OUTPUT); //LED on Model B
+  pinMode(1, OUTPUT); //LED op dit model!   
+  pinMode(2, INPUT);  // Input for button
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(0, HIGH); // powah to the button
+  if (digitalRead(2)){
+    digitalWrite(1, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);
+  }
+  digitalWrite(1, LOW); // turn the LED off by making the voltage LOW
+  Serial.println(digitalRead(2));
+}
