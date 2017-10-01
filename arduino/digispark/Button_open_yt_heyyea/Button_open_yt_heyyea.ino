b91d3619add7f540c64cf389831c04c2f8318fb1
kaliumxyz <martijn@kalium.xyz>
2017-10-02 00:06:20 +0200

    [comments] Dutch to English

--- arduino/digispark/Button_open_yt_heyyea/Button_open_yt_heyyea.ino
+++ arduino/digispark/Button_open_yt_heyyea/Button_open_yt_heyyea.ino
@@ -1,20 +1,22 @@
 // the setup routine runs once when you press reset:
-// Dit gebruikt de hello button layout.
 #include "DigiKeyboard.h"
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(0, OUTPUT); //LED on Model B
-  pinMode(1, OUTPUT); //LED op dit model!   
+  pinMode(1, OUTPUT); //LED on the model I'm using
   pinMode(2, INPUT);  // Input for button
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(0, HIGH); // powah to the button
+  // I was using pin 0 to power the button, you can just use the 5v out
+  // digitalWrite(0, HIGH);
   if (digitalRead(2)){
     DigiKeyboard.update();
     DigiKeyboard.sendKeyStroke(0);
+    // for windows
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+    // wait for the run window to open
     delay(100);
     DigiKeyboard.print("https://www.youtube.com/watch?v=ZZ5LpwO-An4");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
